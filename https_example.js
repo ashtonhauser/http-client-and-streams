17da2ebb2a22b6e9e84d64502df87272b39bb06d
Ashton Hauser <ashton.hauser777@gmail.com>
2018-10-31 02:43:08 +0000

    Ability to get data added

---
+++ https_example.js
@@ -0,0 +1,21 @@
+const https = require('https');
+
+const options = {
+  host: 'www.example.org',
+  path: '/'
+};
+
+let callback = function(response) {
+  console.log('In response handler callback!');
+
+  response.on('data', function(chunk) {
+    console.log(`[-- CHUNK OF LENGTH ${chunk.length} --]`);
+    console.log(chunk.toString());
+  });
+};
+
+console.log('I\'m about to make the request!');
+
+https.request(options, callback).end();
+
+console.log('I\'ve made the request!');
